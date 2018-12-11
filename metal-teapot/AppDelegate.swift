d457641be3261f0008f7bca78e7c75533fae1990
Blake Lockley <bwlockley@gmail.com>
2018-12-11 21:56:08 +1100

    Inital Commit

---
+++ metal-teapot/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  metal-teapot
+//
+//  Created by Blake Lockley on 11/12/18.
+//  Copyright © 2018 Blake Lockley. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
